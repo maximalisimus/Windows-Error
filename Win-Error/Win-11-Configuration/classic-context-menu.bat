43f04c888a2d494cf6e7125884cc1470ad7c462d
maximalis171091@yandex.ru <maximalis171091@yandex.ru>
2025-10-20 09:23:45 +0300

    Update

---
+++ Win-Error/Win-11-Configuration/classic-context-menu.bat
@@ -0,0 +1,12 @@
+@cd/d "%~dp0"
+chcp 65001
+@echo off
+
+reg add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve 2>nul
+SET MYERROR=%ERRORLEVEL%
+echo reg add HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32 /f /ve Error=%MYERROR%
+echo reg add HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32 /f /ve Error=%MYERROR% >> log.txt
+
+chcp 866
+
+pause
